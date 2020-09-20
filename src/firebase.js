f96f763d92c7586d3fbb2a25e3599b90701dd696
Sanishmdhr <sanish.manandhar20@gmail.com>
2020-09-20 10:36:04 +0545

    Initial Config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAKtRUE7HfW2TbFMgsiIPaSqcuoOG3HWm0",
+  authDomain: "covid19-tracker-715a3.firebaseapp.com",
+  databaseURL: "https://covid19-tracker-715a3.firebaseio.com",
+  projectId: "covid19-tracker-715a3",
+  storageBucket: "covid19-tracker-715a3.appspot.com",
+  messagingSenderId: "106606688203",
+  appId: "1:106606688203:web:1f4f2f64ad18ca6516e84e",
+  measurementId: "G-GZ7JVVRJGJ"
+};
